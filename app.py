c18f4dd1cce8dabfb268a6c3403e1ea7ad79d1fd
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-05 20:19:00 -0300

    Creating HTML login structure and endpoint

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request, session
 # configuring application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
