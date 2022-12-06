2c0885dff7e755387a208938328803f19bec1279
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-05 22:19:02 -0300

    Boards route

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request, session
 
+
 # configuring application
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/boards')
+def boards():
+    return render_template('boards.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
