d1ff6662a1e5851672acfdf62480ba5c3fad61e6
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-17 18:56:50 -0300

    Changing architecture to MVC

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,5 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-from app import views
-from app import logged_view
+from app.controllers import views
+from app.controllers import logged_view
