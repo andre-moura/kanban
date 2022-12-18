6d6380b4d76968d1fbb244478b9563efbc8cd66d
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-18 19:58:59 -0300

    Configuring migrations and updating gitignore and requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-db = SQLAlchemy(app=app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.controllers import default
 from app.controllers import logged_view
+from app.models import tables
