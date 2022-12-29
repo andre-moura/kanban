50e08686287b571d06fd85b89853af06f5afe5be
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-12-28 23:11:57 -0300

    Adding list creation logic

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, session, redirect, url_fo
 from flask_login import login_required, current_user
 from app.models.tables import UserProjects, Project, Kanban, KanbanList, Task
 from app import db
-
+from sqlalchemy import func
 
 main = Blueprint('main', __name__)
 
@@ -75,7 +75,24 @@ def create_board():
 @login_required
 def list():
     if request.method == 'POST':
-        pass
+        id_kanban = request.form['kanban_submit']
+        list_name = request.form['list_name']
+        order = db.session.query(func.max(KanbanList.order)).filter_by(id_kanban=id_kanban).one()[0]
+
+        if not order:
+            order = 1
+        else:
+            int(order)
+            order += 1
+            
+        new_kanban_list = KanbanList(
+            name=list_name,
+            id_kanban=id_kanban,
+            order=order
+        )
+        db.session.add(new_kanban_list)
+        db.session.commit()
+        return redirect(url_for('main.kanban', id=id_kanban))
 
 @main.route('/kanban/<id>', methods=['GET', 'POST'])
 @login_required
@@ -86,10 +103,7 @@ def kanban(id):
         for kanban_list in kanban_lists:
             ids_lists.append(kanban_list.id)
         tasks = Task.query.filter(Task.id_list.in_(tuple(ids_lists))).all()
-        return render_template('kanban.html', kanban_lists=kanban_lists, tasks=tasks)
-        
-    if request.method == 'POST':
-        return render_template('kanban.html')
+        return render_template('kanban.html', kanban_lists=kanban_lists, tasks=tasks, id_kanban=id)
 
 
 @main.route('/members')
